c2ac2e64fc813fb79b9c0be3eac8cae6d74dd5d5
Elyahu2020 <elyahu2016@gmail.com>
2021-07-21 08:45:49 -0400

    Metodos de lista

--- index.php
+++ index.php
@@ -8,10 +8,32 @@
 
   echo json_encode($usuarios);*/
 
+  /*carrega um usuario
   $root = new Usuario();
 
   $root->loadById(3);
 
-  echo $root;
+  echo $root;*/
+
+  /*Carrega uma lista de usuarios
+
+  $lista =Usuario::getList();
+
+  echo json_encode($lista);*/
+
+  /*Carrega uma lista de usuarios pelo login
+  $search = Usuario::search("na");
+
+  echo json_encode($search);*/
+
+  //carrega um usuario usando login e a senha
+
+  $usuario = new Usuario();
+
+  $usuario->login("root", "!@#$");
+
+  echo $usuario;
+  
+
 
 ?>
